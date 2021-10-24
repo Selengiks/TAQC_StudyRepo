01e208d55b9fc422334750a5883f62cb7b45e26f
Maksym Herasymchuk <maksiimys1980@gmail.com>
2021-10-24 15:38:45 +0300

    Finished 2nd HW (1-3 tasks). Prepared for 3rd HW (4-6 tasks).

--- src/com/taqc/HW.java
+++ src/com/taqc/HW.java
@@ -1,5 +1,6 @@
 package com.taqc;
 
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class HW {
@@ -20,35 +21,65 @@ public class HW {
             System.out.println("The brick does not go through");
     }
     public static void task2() {
-        Scanner input = new Scanner(System.in);
-        int d = 3;
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input n: ");
+        int n = in.nextInt();
+        while (n <= 0) {
+            System.out.println("Negative number.\nInput valid data: ");
+            n = in.nextInt();
+        }
+        int N;
+        System.out.println("n^2 = " + (Math.pow(n, 2)));
+        N = (int) Math.pow(n, 2);
+
+        System.out.println("2.1.\n");
+
         boolean b = false;
-        System.out.println("Enter n: ");
-        int n = input.nextInt();
-        int N = n*n;
-        System.out.println("n^2 = " + N);
-        while ((N != 0) & (!b)) {
-            if (N % 10 == d)
-                b = true;
-            N = N/10;
+        while ((N != 0) && (!b)) {
+            if (N % 10 == 3) b = true;
+            N = N / 10;
         }
+
         if (b)
-            System.out.println("Digit " + d + " is included in the number " + N);
+            System.out.println("Digit 3 included in " + N);
         else
-            System.out.println("Digit " + d + " doesn't included in the number " + n);
+            System.out.println("Digit 3 isn't included in" + N);
 
-        System.out.print("\nReverse: ");
-        while (n != 0) {
-            int k = n % 10;
-            n = n / 10;
-            System.out.print(k);
+        System.out.println("2.2.\n");
+
+        int reverse_n = 0, tmp, c_n = n;
+        do {
+            tmp = c_n % 10;
+            reverse_n = (reverse_n * 10) + tmp;
+            c_n = c_n / 10;
+        } while (c_n > 0);
+        System.out.println("Reverse number: " +reverse_n);
+
+        System.out.println("2.3.\n");
+
+        int  begin, middle = 0, pos = 1;
+        c_n = n;
+        begin = c_n % 10;
+        c_n = c_n / 10;
+        while (c_n > 9) {
+            begin *= 10;
+            middle += (c_n % 10) * pos;
+            pos *= 10;//10  100
+            c_n /= 10;
         }
+        c_n = (begin * 10) + (middle * 10) + c_n;
+        System.out.println("first and last digits replaced: " + c_n);
 
+        System.out.println("2.4.\n");
 
-        //System.out.print("\nChanged the first and last digits: " + permut);
-
-        //stem.out.print("With adding to the end and beginning 1: " + add);
-
+        int result, first_1 = 10;
+        c_n = n;
+        while (c_n != 0) {
+            c_n /= 10;
+            first_1 *= 10;
+        }
+        result = first_1 + n * 10 + 1;
+        System.out.println("Digit \"1\" included:  " + result);
     }
     public static void task3() {
         Scanner input = new Scanner(System.in);
@@ -78,4 +109,92 @@ public class HW {
         long endTime2   = System.nanoTime();
         System.out.println(("Result: " + total + ". Took time together: " + (endTime2 - startTime2) + "ns"));
     }
+    public static void task4() {
+        Scanner input = new Scanner(System.in);
+        boolean state = true;
+        String name;
+        int year;
+        while(state == true) {
+        System.out.println("Choose 1-4, or 0 to exit\n1.Input personal data.\n2.Output personal data.\n3. Calculate the age.\n4. Change name.");
+        int c = input.nextInt();
+        Person[] personList = new Person[5];
+            switch(c) {
+                case(1):
+                    for(int i = 0; i < 5; i++) {
+                        System.out.print("Name: ");
+                        name = input.next();
+                        System.out.print("Year: ");
+                        year = input.nextInt();
+                        personList[i] = new Person(name, year);
+                    }
+                    break;
+                case(2):
+                    Person.OutData();
+                    break;
+                case(3):
+                    Person.age();
+                    break;
+                case(4):
+                    Person.changeName();
+                    break;
+                case(0):
+                    state = false;
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid data");
+            }
+        }
+
+    }
+    public static void task5() {
+
+    }
+    public static void task6() {
+
+    }
 }
+
+class Person {
+    private static String name;
+    private static int year;
+    static int curYear = Calendar.getInstance().get(Calendar.YEAR);
+    int age;
+
+    public Person()
+    {
+        name = "";
+        year = 0;
+    }
+
+    public Person(String nameVal, int yearVal) {
+        name = nameVal;
+        year = yearVal;
+    }
+
+    static void InData(String name, int year) {
+        Person.name = name;
+        Person.year = year;
+    }
+
+    static void OutData() {
+            System.out.println("Name: " + name + ".\nBirth year: " + year + ".\n");
+    }
+
+    public static void age() {
+        System.out.println("Person age: " + (curYear - year));
+    }
+
+    public static void changeName() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+}
+
--- src/com/taqc/Main.java
+++ src/com/taqc/Main.java
@@ -2,24 +2,21 @@ package com.taqc;
 
 import java.util.Scanner;
 
-
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         while(true) {
-            System.out.println("Choose a task: \n 1, 2, 3 or 0 to exit");
-            var c = input.next();
-            switch(c) {
-                case("1"):
-                    HW.task1();
-                case("2"):
-                    HW.task2();
-                case("3"):
-                    HW.task3();
-                case("0"):
-                    System.exit(0);
-                default:
-                    System.out.println("Incorrect input");
+            System.out.println("Choose a task: \n 1-6 or 0 to exit");
+            String c = input.next();
+            switch (c) {
+                case ("1") -> HW.task1();
+                case ("2") -> HW.task2();
+                case ("3") -> HW.task3();
+                case ("4") -> HW.task4();
+                case ("5") -> HW.task5();
+                case ("6") -> HW.task6();
+                case ("0") -> System.exit(0);
+                default -> System.out.println("Incorrect input");
             }
         }
     }
