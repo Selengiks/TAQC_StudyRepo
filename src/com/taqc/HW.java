94221c8e73fe264b789e0225e4e3015fe453db07
Maksym Herasymchuk <maksiimys1980@gmail.com>
2021-10-27 16:38:19 +0300

    Added task 5 Added task 6 minor fix in task 1-3

--- src/com/taqc/HW.java
+++ src/com/taqc/HW.java
@@ -1,12 +1,11 @@
 package com.taqc;
 
-import java.util.Calendar;
-import java.util.Scanner;
+import java.util.*;
 
 public class HW {
     public static void task1() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter brick params: a, b, c");
+        System.out.println("\nEnter brick params: a, b, c");
         float a = input.nextFloat();
         float b = input.nextFloat();
         float c = input.nextFloat();
@@ -18,7 +17,7 @@ public class HW {
             (c <= x & b <= y) & (b <= x & c <= y))
             System.out.println("The brick goes through the hole");
         else
-            System.out.println("The brick does not go through");
+            System.out.println("The brick does not go through\n");
     }
     public static void task2() {
         Scanner in = new Scanner(System.in);
@@ -32,7 +31,7 @@ public class HW {
         System.out.println("n^2 = " + (Math.pow(n, 2)));
         N = (int) Math.pow(n, 2);
 
-        System.out.println("2.1.\n");
+        System.out.println("\n2.1.");
 
         boolean b = false;
         while ((N != 0) && (!b)) {
@@ -45,7 +44,7 @@ public class HW {
         else
             System.out.println("Digit 3 isn't included in" + N);
 
-        System.out.println("2.2.\n");
+        System.out.println("\n2.2.");
 
         int reverse_n = 0, tmp, c_n = n;
         do {
@@ -55,7 +54,7 @@ public class HW {
         } while (c_n > 0);
         System.out.println("Reverse number: " +reverse_n);
 
-        System.out.println("2.3.\n");
+        System.out.println("\n2.3.");
 
         int  begin, middle = 0, pos = 1;
         c_n = n;
@@ -70,7 +69,7 @@ public class HW {
         c_n = (begin * 10) + (middle * 10) + c_n;
         System.out.println("first and last digits replaced: " + c_n);
 
-        System.out.println("2.4.\n");
+        System.out.println("\n2.4.");
 
         int result, first_1 = 10;
         c_n = n;
@@ -79,22 +78,23 @@ public class HW {
             first_1 *= 10;
         }
         result = first_1 + n * 10 + 1;
-        System.out.println("Digit \"1\" included:  " + result);
+        System.out.println("Digit \"1\" included:  " + result + "\n");
     }
     public static void task3() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter radius: ");
+        System.out.println("Task 3.1.\nEnter radius: ");
         int r = input.nextInt();
         var p = 2 * Math.PI * r;
         var a = Math.PI * Math.pow(r, 2);
         System.out.println("Circle P: " + p + "\nCircle A: " + a);
 
-        System.out.println("What is your name?");
+        System.out.println("\nTssk 3.2.\nWhat is your name?");
         String name = input.next();
         System.out.println("Where are you live, " + name + '?');
-        String adress = input.next();
-        System.out.println("Hello, " + name + " from " + adress + '.');
+        String adr = input.next();
+        System.out.println("Hello, " + name + " from " + adr + '.');
 
+        System.out.println("\nTask 3.3. ");
         long startTime1 = System.nanoTime();
         int c1 = 440, c2 = 320, c3 = 760;
         int t1 = 340, t2 = 430, t3 = 560;
@@ -103,92 +103,184 @@ public class HW {
         int total3 = t3 * c3;
         int all = total1 + total2 + total3;
         long endTime1   = System.nanoTime();
-        System.out.println(("Result: " + total1 + ", " + total2 + ", " + total3 + " = " + all + ". Took time separately: " + (endTime1 - startTime1) + "ns"));
+        System.out.println(("Result:\nTotal cost c1: " + total1 + "$,\nTotal cost c2: " + total2 + "$,\nTotal cost c3: " + total3 + "$.\nTotal cost: " + all + "$. Took time separately: " + (endTime1 - startTime1) + "ns"));
         long startTime2 = System.nanoTime();
         int total = t1 * c1 + t2 * c2 + t3 * c3;
         long endTime2   = System.nanoTime();
-        System.out.println(("Result: " + total + ". Took time together: " + (endTime2 - startTime2) + "ns"));
+        System.out.println(("Result:\nTotal cost: " + total + "$.\nTook time together: " + (endTime2 - startTime2) + "ns"));
     }
     public static void task4() {
-        Scanner input = new Scanner(System.in);
-        boolean state = true;
-        String name;
-        int year;
-        while(state == true) {
-        System.out.println("Choose 1-4, or 0 to exit\n1.Input personal data.\n2.Output personal data.\n3. Calculate the age.\n4. Change name.");
-        int c = input.nextInt();
-        Person[] personList = new Person[5];
-            switch(c) {
-                case(1):
-                    for(int i = 0; i < 5; i++) {
-                        System.out.print("Name: ");
-                        name = input.next();
-                        System.out.print("Year: ");
-                        year = input.nextInt();
-                        personList[i] = new Person(name, year);
-                    }
-                    break;
-                case(2):
-                    Person.OutData();
-                    break;
-                case(3):
-                    Person.age();
-                    break;
-                case(4):
-                    Person.changeName();
-                    break;
-                case(0):
-                    state = false;
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid data");
-            }
-        }
+        Person person1=new Person();
+        person1.Input();
+        person1.output();
+        person1.Age();
+        person1.ChangeName();
+        person1.output();
+        System.out.print("\n");
+
+        Person person2=new Person();
+        person2.Input();
+        person2.output();
+        person2.Age();
+        person2.ChangeName();
+        person2.output();
+        System.out.print("\n");
+
+        Person person3=new Person();
+        person3.Input();
+        person3.output();
+        person3.Age();
+        person3.ChangeName();
+        person3.output();
+        System.out.print("\n");
+
+
+        Person person4=new Person();
+        person4.Input();
+        person4.output();
+        person4.Age();
+        person4.ChangeName();
+        person4.output();
+        System.out.print("\n");
+
+        Person person5=new Person();
+        person5.Input();
+        person5.output();
+        person5.Age();
+        person5.ChangeName();
+        person5.output();
 
     }
     public static void task5() {
+        Scanner input = new Scanner(System.in);
+        Fifth func = new Fifth();
+        System.out.print("\nTask 5.1.1\nInput first float number: ");
+        float n1 = input.nextFloat();
+        func.rangeCompare(n1);
 
+        System.out.print("Input second float number: ");
+        float n2 = input.nextFloat();
+        func.rangeCompare(n2);
+
+        System.out.print("Input third float number: ");
+        float n3 = input.nextFloat();
+        func.rangeCompare(n3);
+
+        System.out.println("\nTask 5.1.2\n");
+        System.out.println("Enter array length: ");
+        int size = input.nextInt();
+        int[] array = new int[size];
+        System.out.println("Insert array elements: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+        System.out.println("Maximum value in the array is: "+ func.max(array));
+        System.out.println("Minimum value in the array is: "+ func.min(array));
+
+        System.out.print("\nTask 5.1.3\nInput code (400-405): ");
+        int code = input.nextInt();
+        func.HTTPError(code);
+
+        System.out.println("\nTask 5.2\n");
+        Dog dog1 = new Dog("Verdana", Breeds.MALAMUTE, 11);
+        Dog dog2 = new Dog("Kiki", Breeds.POODLE, 2);
+        Dog dog3 = new Dog("Vasyl", Breeds.GOLDEN_RETRIEVER, 4);
+
+        String new_name;
+        if (dog1.getName().equals(dog2.getName())) {
+            System.out.println("Please rename the dog," + dog2.getName() + " with age " + dog2.getAge() + ", name already taken. New name: ");
+            new_name = input.next();
+            dog2.setName(new_name);
+        } else if (dog2.getName().equals(dog3.getName())) {
+            System.out.println("Please rename the dog, " + dog3.getName() + " with age " + dog3.getAge() + ", name already taken. New name: ");
+            new_name = input.next();
+            dog3.setName(new_name);
+        } else if (dog3.getName().equals(dog1.getName())) {
+            System.out.println("Please rename the dog, " + dog3.getName() + " with age " + dog3.getAge() + ", name already taken. New name: ");
+            new_name = input.next();
+            dog3.setName(new_name);
+        }
+
+        if (dog1.getAge() > dog2.getAge() && dog1.getAge() > dog3.getAge()) {
+            dog1.output();
+        } else if (dog2.getAge() > dog1.getAge() && dog2.getAge() > dog3.getAge()) {
+            dog2.output();
+        } else if (dog3.getAge() > dog1.getAge() && dog3.getAge() > dog1.getAge()) {
+            dog3.output();
+        }
+
+        if (dog1.getAge() == dog2.getAge() && dog1.getAge() > dog3.getAge()) {
+            dog1.output();
+            dog2.output();
+        } else if (dog2.getAge() == dog3.getAge() && dog2.getAge() > dog1.getAge()) {
+            dog2.output();
+            dog3.output();
+        } else if (dog3.getAge() == dog1.getAge() && dog3.getAge() > dog2.getAge()) {
+            dog1.output();
+            dog3.output();
+        }
     }
     public static void task6() {
+        new User("Vitaliy", 35, Sex.MALE);
+        new User("Masha", 34, Sex.FEMALE);
+        new User("Ariana", 7, Sex.FEMALE);
 
+
+        System.out.println("All users:");
+        User.getAllUsers().forEach(System.out::println);
+        System.out.println("All users: MALE");
+        User.getAllUsers(Sex.MALE).forEach(System.out::println);
+        System.out.println("All users: FEMALE");
+        User.getAllUsers(Sex.FEMALE).forEach(System.out::println);
+        System.out.println("================================================");
+        System.out.println("       all users: " + User.getHowManyUsers());
+        System.out.println("  all users MALE: " + User.getHowManyUsers(Sex.MALE));
+        System.out.println("all users FEMALE: " + User.getHowManyUsers(Sex.FEMALE));
+        System.out.println("================================================");
+        System.out.println("       total users age: " + User.getAllAgeUsers());
+        System.out.println("  total users age MALE: " + User.getAllAgeUsers(Sex.MALE));
+        System.out.println("total users age FEMALE: " + User.getAllAgeUsers(Sex.FEMALE));
+        System.out.println("================================================");
+        System.out.println("       average users age: " + User.getAverageAgeOfAllUsers());
+        System.out.println("  average users age MALE: " + User.getAverageAgeOfAllUsers(Sex.MALE));
+        System.out.println("average users age FEMALE: " + User.getAverageAgeOfAllUsers(Sex.FEMALE));
+        System.out.println("================================================");
     }
+
 }
 
 class Person {
-    private static String name;
-    private static int year;
-    static int curYear = Calendar.getInstance().get(Calendar.YEAR);
-    int age;
+    private String name;
+    private int year;
+    private final int curDate = Calendar.getInstance().get(Calendar.YEAR);
 
-    public Person()
-    {
-        name = "";
-        year = 0;
+    Person(){}
+    Person(String name,int birthYear){
+        this.name=name;
+        this.year =birthYear;
     }
 
-    public Person(String nameVal, int yearVal) {
-        name = nameVal;
-        year = yearVal;
+    void Age(){
+        System.out.println("Age: ");
+        System.out.println(curDate - this.year);
     }
-
-    static void InData(String name, int year) {
-        Person.name = name;
-        Person.year = year;
-    }
-
-    static void OutData() {
-            System.out.println("Name: " + name + ".\nBirth year: " + year + ".\n");
-    }
-
-    public static void age() {
-        System.out.println("Person age: " + (curYear - year));
-    }
-
-    public static void changeName() {
+    void Input(){
+        System.out.print("Enter Name: ");
+        Scanner sc =new Scanner(System.in);
+        name = sc.nextLine();
+        System.out.print("Enter birthYear: ");
+        year = sc.nextInt();
 
     }
-
-    public String getName() {
+    void output(){
+        System.out.println("Name:"+ name +" birthYear: " + year);
+    }
+    void ChangeName(){
+        System.out.print("Change Name: ");
+        Scanner sc =new Scanner(System.in);
+        name = sc.nextLine();
+    }
+    public  String getName() {
         return name;
     }
 
@@ -196,5 +288,119 @@ class Person {
         return year;
     }
 
-}
+    public void setName(String name) {
+        this.name = name;
+    }
 
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+
+}
+class Fifth {
+    public boolean rangeCompare(float num) {
+        if ((num >= -5 && num <= 5)) {
+            System.out.println("Number is belong to the range [-5;5]");
+            return true;
+        } else {
+            System.out.println("Number isn't belong to the range [-5;5]");
+            return false;
+        }
+    }
+
+    public int max(int[] array) {
+        int max = 0;
+
+        for (int j : array) {
+            if (j > max) {
+                max = j;
+            }
+        }
+        return max;
+    }
+
+    public int min(int[] array) {
+        int min = array[0];
+
+        for (int j : array) {
+            if (j < min) {
+                min = j;
+            }
+        }
+        return min;
+    }
+        public void HTTPError (final int code) {
+            HTTPError[] errors = HTTPError.values();
+            for (HTTPError er : errors) {
+                if (code == er.getCode())
+                    System.out.println(er);
+            }
+        }
+}
+enum HTTPError {
+    Bad_Request(400), Unauthorized(401), Payment_Required(402), Forbidden(403), Not_Found(404), Method_Not_Allowed(405);
+
+    private final int code;
+
+    HTTPError(int code) {
+        this.code = code;
+    }
+
+    public final int getCode() {
+        return code;
+    }
+}
+class Dog {
+    private String name;
+    private Breeds breed;
+    private int age;
+
+    public Dog(String name, Breeds breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        if (age < 0) {
+            this.age = 0;
+        } else this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Breeds getBreed() {
+        return breed;
+    }
+
+    public void setBreed(Breeds breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            this.age = 0;
+        } else this.age = age;
+    }
+
+    public void output() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Breed: " + this.breed);
+        System.out.println("Age: " + this.age);
+    }
+}
+enum Breeds {
+    AKITA,
+    POODLE,
+    DACHSHUND,
+    MALAMUTE,
+    GOLDEN_RETRIEVER,
+    SHIBU_INU
+}
